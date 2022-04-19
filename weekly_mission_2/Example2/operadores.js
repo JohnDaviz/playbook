2c16a68b86aafed59fb90b7302716249c9accfee
Jhonatan Antonio Hernandez Sarmiento <john_daviz_hdz@hotmail.com>
2022-04-18 19:27:48 -0500

    Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas

--- weekly_mission_2/Example2/operadores.js
+++ weekly_mission_2/Example2/operadores.js
@@ -32,3 +32,11 @@ const toLocaleLowerCase = names.map((name) => name.toLocaleLowerCase())
 
 console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
 console.log(toLocaleLowerCase)
+
+// Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas
+const countries6 = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+const countriesFirstThreeLetters = countries6.map((country) =>
+  country.toUpperCase().slice(0, 3) // el método slice obtiene solo la longitud marcada del string
+)
+console.log("Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas")
+console.log(countriesFirstThreeLetters)
