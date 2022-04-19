909cafb6efc40784af414b2b65932b835332d0c5
Jhonatan Antonio Hernandez Sarmiento <john_daviz_hdz@hotmail.com>
2022-04-18 19:37:20 -0500

    Ejemplo 16: Ordenando una lista de objetos

--- weekly_mission_2/Example2/operadores.js
+++ weekly_mission_2/Example2/operadores.js
@@ -111,3 +111,20 @@ console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSo
 const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
 console.log("Ejemplo 15: Elementos ordernados usando SORT")
 console.log(products.sort())
+
+// Ejemplo 16: Ordenando una lista de objetos
+const users = [
+    { name: 'A', age: 150 },
+    { name: 'B', age: 50 },
+    { name: 'C', age: 100 },
+    { name: 'D', age: 22 },
+  ]
+  
+  users.sort((a, b) => { // podemos invocar una función
+    if (a.age < b.age) return -1
+    if (a.age > b.age) return 1
+    return 0
+  })
+  
+  console.log("Ejemplo 16: Ordenando una lista de objetos por la edad")
+  console.log(users) // sorted ascending
