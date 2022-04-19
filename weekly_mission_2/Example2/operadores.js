36a008d07967a005fea0e2b1a002c92f504bf757
Jhonatan Antonio Hernandez Sarmiento <john_daviz_hdz@hotmail.com>
2022-04-18 19:35:42 -0500

    Ejemplo 15: Uso de sort para ordenar elementos

--- weekly_mission_2/Example2/operadores.js
+++ weekly_mission_2/Example2/operadores.js
@@ -106,3 +106,8 @@ const bools = [true, true, false, true]
 // Uso de Some para ver si al menos uno de los elementos es false
 const areSomeTrue = bools.some((b) =>  b === false)
 console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue) //true
+
+//Ejemplo 15: Uso de sort para ordenar elementos
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort())
